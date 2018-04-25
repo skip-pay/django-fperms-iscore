99de895ef1620f86528f62e0923639b57d181080
Petr Olah <djangoguru@gmail.com>
2018-04-25 14:05:48 +0200

    updated fperms version

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        "django-fperms==0.4.0",
+        "django-fperms==0.4.2",
         "django-is-core==2.11.1",
     ],
     license="MIT",
